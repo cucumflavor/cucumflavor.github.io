f6fdad9741ac7547c10297884e6fc0c8e9015e8e
cucumflavor <laohg1234@aliyun.com>
2025-02-10 22:51:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundImage = "url('https://www.loliapi.com/acg/')";
+
+document.getElementById("forumBtn").addEventListener("click", function() {
+    window.location.href = "http://forum.flavor.ink";
+});
+
+document.getElementById("shopBtn").addEventListener("click", function() {
+    window.location.href = "http://shop.flavor.ink";
+});
+
+document.getElementById("mainBtn").addEventListener("click", function() {
+    window.location.href = "https://lahg.skyman.cloud";
+});
