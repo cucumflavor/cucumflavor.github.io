60214bb9196bacc15ec2651d49ef9d64935b7b52
cucumflavor <laohg1234@aliyun.com>
2025-02-20 20:58:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.body.style.backgroundImage = "url('https://www.loliapi.com/acg/')";
 
 document.getElementById("forumBtn").addEventListener("click", function() {
-    window.location.href = "http://forum.flavor.ink";
+    window.location.href = "https://forum.flavor.ink";
 });
 
 document.getElementById("shopBtn").addEventListener("click", function() {
-    window.location.href = "http://shop.flavor.ink";
+    window.location.href = "https://shop.flavor.ink";
 });
 
 document.getElementById("mainBtn").addEventListener("click", function() {
-    window.location.href = "https://lahg.skyman.cloud";
+    window.location.href = "https://main.flavor.ink";
 });
