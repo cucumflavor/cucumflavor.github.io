b919bb9b116f276d790520bc9915ff82abf93a93
cucumflavor <laohg1234@aliyun.com>
2025-05-16 21:52:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ document.getElementById("forumBtn").addEventListener("click", function() {
     window.location.href = "https://forum.flavor.ink";
 });
 
-document.getElementById("shopBtn").addEventListener("click", function() {
-    window.location.href = "https://shop.flavor.ink";
+document.getElementById("cloudBtn").addEventListener("click", function() {
+    window.location.href = "https://cloud.flavor.ink";
 });
 
 document.getElementById("mainBtn").addEventListener("click", function() {
     window.location.href = "https://main.flavor.ink";
 });
+
+document.getElementById("hostBtn").addEventListener("click", function() {
+    window.location.href = "https://host.flavor.ink";
+});
+
+document.getElementById("fikBtn").addEventListener("click", function() {
+    window.location.href = "https://shop.fiker.top";
+});
+
+
